1a7029d0a3c91aa71736d2983131731b8ab861a0
Tomas Ferrari <tferrari.92@gmail.com>
2023-10-11 18:42:51 +0200

    instrument backend

--- my-app/backend/app.js
+++ my-app/backend/app.js
@@ -1,6 +1,6 @@
 const { init } = require('./tracer')
 const api = require('@opentelemetry/api')
-init('demo-node-service', 'development') // calling tracer with service name and environment to view in jaegerui
+init('my-app-backend', 'development') // calling tracer with service name and environment to view in jaegerui
 
 const express = require("express");
 const Redis = require("ioredis");
@@ -36,6 +36,7 @@ async function getAndIncrementVisitorCount() {
 // API endpoint to retrieve visitor count
 app.get("/", async (req, res) => {
   try {
+    randomWait();
     const visitorCount = await getAndIncrementVisitorCount();
     res.json({ count: visitorCount });
   } catch (error) {
@@ -48,3 +49,24 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+
+function randomWait() {
+  // Generate a random number between 0 and 1
+  const randomValue = Math.random();
+
+  if (randomValue < 0.5) {
+    // If the random value is less than 0.5, wait for 2 seconds
+    setTimeout(() => {
+      console.log('Waited for 2 seconds.');
+      callback();
+    }, 2000);
+  } else {
+    // If the random value is greater than or equal to 0.5, don't wait
+    console.log('No wait.');
+    callback();
+  }
+}
+
+
