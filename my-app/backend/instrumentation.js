88fa3657d1365a78a4ece6a3698d500365514895
Tomas Ferrari <tferrari.92@gmail.com>
2023-10-11 16:31:18 +0200

    instrument backend

--- my-app/backend/instrumentation.js
+++ my-app/backend/instrumentation.js
@@ -16,13 +16,15 @@ const sdk = new opentelemetry.NodeSDK({
   traceExporter: new OTLPTraceExporter({
     // optional - default url is http://localhost:4318/v1/traces
     // url: '<your-otlp-endpoint>/v1/traces',
-    url: process.env.JAEGER_ENDPOINT,
+    // url: process.env.JAEGER_ENDPOINT,
+    url: "http://jaeger-collector.observability:14268/api/traces",
     // optional - collection of custom headers to be sent with each request, empty by default
     // headers: {},
   }),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter({
       // url: '<your-otlp-endpoint>/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
+      url: "http://jaeger-collector.observability:14268/api/metrics",
       // headers: {}, // an optional object containing custom headers to be sent with each request
       // concurrencyLimit: 1, // an optional limit on pending requests
     }),
