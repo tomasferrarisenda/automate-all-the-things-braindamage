6aebed83d09256a356393cb2df8146dbc8cdccf4
Tomas Ferrari <tferrari.92@gmail.com>
2024-05-15 19:46:01 +0200

    Refactor security group YAML file for meme-web backend

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -6,9 +6,9 @@ output "database_subnet_id" {
   value = aws_subnet.private-subnet-c.id
 }
 
-output "database_security_group_id" {
-  value = aws_security_group.databases.id
-}
+# output "database_security_group_id" {
+#   value = aws_security_group.databases.id
+# }
 
 # output "elasticache_dev_primary_endpoint_address" {
 #   value = aws_elasticache_replication_group.elasticache-replication-group-dev.primary_endpoint_address
